caeaa2faf29de795ea89a5bc833db0fb7bcc1a92
kidde60 <kgeorgewilliam60@gmail.com>
2022-09-16 18:03:31 +0300

    Change classes to functions

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,53 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-/* eslint-disable react/destructuring-assignment */
-class MyComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { total: 0, next: null, operation: null };
-    this.handleEvent = this.handleEvent.bind(this);
-  }
+const MyComponent = () => {
+  const [state, setState] = useState('0');
 
-  handleEvent(e) {
-    // eslint-disable-next-line react/no-access-state-in-setstate
-    this.setState(calculate(this.state, e.target.textContent));
-  }
+  const handleEvent = (e) => {
+    setState((state) => calculate(state, e.target.textContent));
+  };
 
-  render() {
-    return (
-      <div className="Calculator">
-        <div className="display">
-          <span>
-            {this.state.total}
-            {this.state.operation}
-            {this.state.next}
-          </span>
-        </div>
-        <div className="buttons">
-          <button type="button" onClick={this.handleEvent}>AC</button>
-          <button type="button" onClick={this.handleEvent}>+/-</button>
-          <button type="button" onClick={this.handleEvent}>%</button>
-          <button type="button" onClick={this.handleEvent} className="operator">/</button>
-          <button type="button" onClick={this.handleEvent}>7</button>
-          <button type="button" onClick={this.handleEvent}>8</button>
-          <button type="button" onClick={this.handleEvent}>9</button>
-          <button type="button" onClick={this.handleEvent} className="operator">X</button>
-          <button type="button" onClick={this.handleEvent}>4</button>
-          <button type="button" onClick={this.handleEvent}>5</button>
-          <button type="button" onClick={this.handleEvent}>6</button>
-          <button type="button" onClick={this.handleEvent} className="operator">-</button>
-          <button type="button" onClick={this.handleEvent}>1</button>
-          <button type="button" onClick={this.handleEvent}>2</button>
-          <button type="button" onClick={this.handleEvent}>3</button>
-          <button type="button" onClick={this.handleEvent} className="operator">+</button>
-          <button type="button" onClick={this.handleEvent} className="zero">0</button>
-          <button type="button" onClick={this.handleEvent}>.</button>
-          <button type="button" onClick={this.handleEvent} className="operator">=</button>
-        </div>
+  return (
+    <div className="Calculator">
+      <div className="display">
+        <span>
+          {state.next || state.operator || state.total || 0}
+        </span>
       </div>
-    );
-  }
-}
+      <div className="buttons">
+        <button type="button" onClick={handleEvent}>AC</button>
+        <button type="button" onClick={handleEvent}>+/-</button>
+        <button type="button" onClick={handleEvent}>%</button>
+        <button type="button" onClick={handleEvent} className="operator">/</button>
+        <button type="button" onClick={handleEvent}>7</button>
+        <button type="button" onClick={handleEvent}>8</button>
+        <button type="button" onClick={handleEvent}>9</button>
+        <button type="button" onClick={handleEvent} className="operator">X</button>
+        <button type="button" onClick={handleEvent}>4</button>
+        <button type="button" onClick={handleEvent}>5</button>
+        <button type="button" onClick={handleEvent}>6</button>
+        <button type="button" onClick={handleEvent} className="operator">-</button>
+        <button type="button" onClick={handleEvent}>1</button>
+        <button type="button" onClick={handleEvent}>2</button>
+        <button type="button" onClick={handleEvent}>3</button>
+        <button type="button" onClick={handleEvent} className="operator">+</button>
+        <button type="button" onClick={handleEvent} className="zero">0</button>
+        <button type="button" onClick={handleEvent}>.</button>
+        <button type="button" onClick={handleEvent} className="operator">=</button>
+      </div>
+    </div>
+  );
+};
 
 export default MyComponent;

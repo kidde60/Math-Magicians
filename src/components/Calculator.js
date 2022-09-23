6cf9e9dee397d25ff65051384c4456ffdda61a40
kidde60 <kgeorgewilliam60@gmail.com>
2022-09-23 23:52:48 +0300

    Edit calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,29 +7,34 @@ const Calculator = () => {
     setState((state) => calculate(state, event.target.textContent));
   };
   return (
-    <div className="cal-grid">
-      <button type="button" className="workspace">
-        {state.next || state.total || 0}
-      </button>
-      <button type="button" onClick={handleEvent} className="cal-Btn">AC</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn">+/-</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn">%</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn operator">÷</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn">7</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn">8</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn">9</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn operator">×</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn">4</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn">5</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn">6</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn operator">-</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn">1</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn">2</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn">3</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn operator">+</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn zero">0</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn">.</button>
-      <button type="button" onClick={handleEvent} className="cal-Btn operator">=</button>
+    <div className="Calculator">
+
+      <div className="display">
+        <span>
+          {state.next || state.operator || state.total || 0}
+        </span>
+      </div>
+      <div className="buttons">
+        <button type="button" onClick={handleEvent}>AC</button>
+        <button type="button" onClick={handleEvent}>+/-</button>
+        <button type="button" onClick={handleEvent}>%</button>
+        <button type="button" onClick={handleEvent} className="operator">/</button>
+        <button type="button" onClick={handleEvent}>7</button>
+        <button type="button" onClick={handleEvent}>8</button>
+        <button type="button" onClick={handleEvent}>9</button>
+        <button type="button" onClick={handleEvent} className="operator">X</button>
+        <button type="button" onClick={handleEvent}>4</button>
+        <button type="button" onClick={handleEvent}>5</button>
+        <button type="button" onClick={handleEvent}>6</button>
+        <button type="button" onClick={handleEvent} className="operator">-</button>
+        <button type="button" onClick={handleEvent}>1</button>
+        <button type="button" onClick={handleEvent}>2</button>
+        <button type="button" onClick={handleEvent}>3</button>
+        <button type="button" onClick={handleEvent} className="operator">+</button>
+        <button type="button" onClick={handleEvent} className="zero">0</button>
+        <button type="button" onClick={handleEvent}>.</button>
+        <button type="button" onClick={handleEvent} className="operator">=</button>
+      </div>
     </div>
 
   );

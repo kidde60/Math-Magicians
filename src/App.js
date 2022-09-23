591fb96922938dfb0c19c6de114c151877475bee
kidde60 <kgeorgewilliam60@gmail.com>
2022-09-23 22:27:06 +0300

    Import about and qoute pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import MyComponent from './components/Calculator';
+import { Link, Routes, Route } from 'react-router-dom';
+import CalculatorUI from './pages/CalculatorUI';
+import Home from './pages/Home';
+import Quote from './pages/Quotes';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
-    return (
-      <MyComponent />
-    );
-  }
-}
-
+const App = () => (
+  // Create the navigation bar of the App
+  <header>
+    <nav>
+      <h1 className="app-title">Math Magicians</h1>
+      <ul className="menu-list">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li> | </li>
+        <li>
+          <Link to="/calculator">Calculator</Link>
+        </li>
+        <li> | </li>
+        <li>
+          <Link to="/quote">Quote</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<CalculatorUI />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </header>
+);
 export default App;

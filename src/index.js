4da56afe41e1be7ed589210d57afce94e9a8d21f
kidde60 <kgeorgewilliam60@gmail.com>
2022-09-23 22:16:32 +0300

    Add function components

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
@@ -6,6 +7,8 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
